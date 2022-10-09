a1000d9af3e119063ccf27243e4e3c57e6841637
viniwell <viniwell280709@gmail.com>
2022-10-09 13:26:40 +0100

    E.py

---
+++ E.py
@@ -0,0 +1,22 @@
+n=int(input('input amount-->'))
+dict=dict(math=0,
+phisics=0,
+python=0)
+
+for i in range (n):
+    list=[str(s) for s in input('list with marks-->').split()]
+    marks_count=3
+    for i in range(len(list)):
+        if i>0:
+            if i==1:
+                list[i]=int(list[i])
+                dict['math']+=list[i]
+            elif i==2:
+                list[i]=int(list[i])
+                dict['phisics']+=list[i]  
+            elif i==3:
+                list[i]=int(list[i])
+                dict['python']+=list[i]               
+for i in dict.keys():
+    dict[i]=(f'{(dict[i]/3):.2f}')
+print(dict['math'], dict['phisics'],dict['python'])
