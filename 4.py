b8d7f67e927c22a4b14cede28d46afa4dc2d2e62
viniwell <viniwell280709@gmail.com>
2022-10-15 13:57:27 +0100

    changed d.py(4.py)

--- 4.py
+++ 4.py
@@ -9,7 +9,11 @@ for i in range (n):
         if i>0:
             list[i]=int(list[i])
     dict[list[0]]=list[1:]
+
+
 for i in dict.keys():
     dict[i]=(f'{((sum(x for x in dict[i]))/marks_count):.2f}')
+
+print('-'*30) 
 for i in magazine:
     print(i, dict[i])
