e6ba14c8ce0210846320f3b83e754013d06ae0fd
viniwell <viniwell280709@gmail.com>
2022-10-15 14:41:22 +0100

    f.py

--- F.py
+++ F.py
@@ -1,17 +1,17 @@
 n=int(input('input amount-->'))
-dict={}
-magazine=[]
+marks_count=3
+pupils=[]
 for i in range (n):
-    list=[str(s) for s in input('list with marks-->').split()]
-    magazine.append(list[0])
-    marks_count=len(list)-1
-    for i in range(len(list)):
-        if i>0:
-            list[i]=int(list[i])
-    dict[list[0]]=list[1:]
-for i in dict.keys():
-    marks=' '.join(str(x) for x in dict[i])
-    dict[i]=(f'{marks} {((sum([x for x in dict[i]]))/marks_count):.2f}')
-for i in sorted(dict.keys()):
-    print(i,dict[i])
+    pupil=input().split()
+    pupil[1:]=[int(i) for i in pupil[1:]]
+    pupil.insert(0, -sum(pupil[1:])/marks_count)
+    pupils.append(pupil)
+
+print('-'*30)
+pupils.sort()
+for pupil in pupils:
+    print(pupil[1],
+    ' '.join(str(mark) for mark in pupil[2:]),
+    f'{-pupil[0]:.2f}' 
+    ) 
 
--- I.py
+++ I.py
@@ -1,13 +1,21 @@
 a=[]
-n=input().split()
-while n[0]!='exit':
+
+key=0
+while key==0:
+    n=input().split()
     if n[0]=='push':
         a.append(n[1])
+        print('ok')
     elif n[0]=='pop':
         print(a[len(a)-1])
-        a.pop(a[len(a)-1])
+        a.pop(int(a[len(a)-1]))
     elif n[0]=='back':
-        a.pop(a[len(a)-1])
+        print(a[len(a)-1])
     elif n[0]=='size':
         print(len(a))
-    elif n[0]
+    elif n[0]=='clear':
+        a.clear()
+        print('ok')
+    elif n[0]=='exit':
+        print('bye')
+        key=1
